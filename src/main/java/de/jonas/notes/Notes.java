b0445571f300f029568e8ce35c79c44c902fcf4f
GemueseHasser <jonas.lobe.2006@outlook.com>
2025-04-23 10:07:41 +0200

    Initial commit

---
+++ src/main/java/de/jonas/notes/Notes.java
@@ -0,0 +1,11 @@
+package de.jonas.notes;
+
+import org.jetbrains.annotations.NotNull;
+
+public class Notes {
+
+    public static void main(@NotNull final String @NotNull [] args) {
+        
+    }
+
+}
